ec3d6b0b1c0eaacaca9021949904893570f4e201
Moussa <ingmoussacly@hotmail.com>
2023-12-16 11:44:13 +0000

    Update 101

--- test1.py
+++ test1.py
@@ -3,3 +3,11 @@ print("""#####################################
     ELECTROCARDIOGRAMME
     ##########################################""")
 print("Bonne chance pour le projet!")
+
+"""
+    We are restarting working on the project
+    Hopefully we will a lot of progress from now on
+
+"""
+
+print("Let's do it")
